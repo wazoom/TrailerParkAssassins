b9d315d628b2bf03b23f4f7f6ae2b131cc748ff9
wazoom <wazoom909@yahoo.com>
2016-03-30 13:09:58 -0400

    Unity Files added
    The first commit with the unity3d files

---
+++ Assets/scripts/EnemyBehavior.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class EnemyBehavior : MonoBehaviour {
+
+    public int hp = 1;
+
+    // maximum linear speed
+    public float speed = 5;
+    public float moveForce = 1500;
+    public Sprite deadSprite;
+
+    // layers the enemy can't see through
+    public LayerMask wallLayers;
+
+    private bool playerInROI = false;
+
+    private Vector2 heading;
+    private Rigidbody2D rb2D;
+    private SpriteRenderer spr;
+    private Transform player;
+
+	void Start () {
+        rb2D = GetComponent<Rigidbody2D>();
+        spr = GetComponent<SpriteRenderer>();
+	}
+
+    void Update()
+    {
+        if(playerInROI)
+        Debug.DrawRay(transform.position, -heading);
+    }
+
+    void FixedUpdate()
+    {
+        if (hp > 0 && playerInROI)
+        {
+            if (!Physics2D.Linecast(transform.position, player.position, wallLayers))
+            {
+                heading = transform.position - player.position;
+                float angle = 0;
+                if (heading.x != 0)
+                    angle = Mathf.Atan2(heading.x, -heading.y) * Mathf.Rad2Deg;
+                rb2D.rotation = angle;
+
+                Mathf.Clamp(rb2D.angularVelocity, 0, 0);
+                rb2D.AddRelativeForce(Vector2.up * moveForce * Time.deltaTime);
+                Vector2.ClampMagnitude(rb2D.velocity, speed);
+            }
+        } else if (hp == 0) {
+            rb2D.isKinematic = true;
+            spr.sprite = deadSprite;
+            Destroy(GetComponent<Collider2D>());
+            GetComponent<EnemyBehavior>().enabled = false;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            if(!player) player = other.transform;
+            playerInROI = true;
+            Debug.Log(gameObject.name + "ROI: " +  playerInROI);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            playerInROI = false;
+            Debug.Log(gameObject.name + "ROI: " +  playerInROI);
+        }
+    }
+}
---
+++ Assets/scripts/PlayerMoveTest.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMoveTest : MonoBehaviour {
+
+    public float speed = 5;
+    public float moveForce = 10;
+    private Rigidbody2D rb2D;
+
+	// Use this for initialization
+	void Start () {
+        rb2D = GetComponent<Rigidbody2D>();
+	}
+	
+	void FixedUpdate () {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        Vector2 f;
+        f.x = x * moveForce * Time.deltaTime;
+        f.y = y * moveForce * Time.deltaTime;
+
+        rb2D.AddRelativeForce(f);
+        Vector2.ClampMagnitude(rb2D.velocity, speed);
+	}
+}
